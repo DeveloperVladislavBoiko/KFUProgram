3db54979c480ce4305deaf6c7ef588e43a75a6d5
Vladislav Boiko <130917759+DeveloperVladislavBoiko@users.noreply.github.com>
2025-07-17 10:18:38 +0300

    Update task 101 demidovich.py

--- first-year practice/Task 1/task 101 demidovich.py
+++ first-year practice/Task 1/task 101 demidovich.py
@@ -1,8 +1,8 @@
 def numerical_sequence_101(n_max=10**6):
     x_large = 1 - 1 / n_max
-    inf_xn = 0  # достигается при n=1
-    sup_xn = x_large  # стремится к 1
-    limit = x_large  # предел равен 1
+    inf_xn = 0  
+    sup_xn = x_large  
+    limit = x_large 
     limsup = limit
     return inf_xn, sup_xn, limit, limsup
 
